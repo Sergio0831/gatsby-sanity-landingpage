1a088435702b10ab3e7c051cd95b2e78fb12e349
Sergio0831 <sergejs.ivcenko@gmail.com>
2021-08-19 15:26:56 +0100

    Implemented Gatsby Sanity connection

---
+++ studio/schemas/fastFact.js
@@ -0,0 +1,12 @@
+export default {
+  title: "Fast facts",
+  name: "fastFact",
+  type: "document",
+  fields: [
+    {
+      title: "Facts",
+      name: "fact",
+      type: "string"
+    }
+  ]
+};
